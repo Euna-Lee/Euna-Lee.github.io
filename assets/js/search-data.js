238d25307f854a73ce4286259d7f02214c9198f5
Euna-Lee <74212964+Euna-Lee@users.noreply.github.com>
2026-08-24 17:29:57 +0000

    Deploying to gh-pages from @ Euna-Lee/Euna-Lee.github.io@03bb4b7634c13c1aa5a52da116b86ce71b7bbd99 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,33 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "projects",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
